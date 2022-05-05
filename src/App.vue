e005b0881c11fa0d210e3435b73b23b13b8bac41
Mateus Paiva <matteusp@alu.ufc.br>
2022-05-05 17:02:31 -0300

    adição de hierarquia de componentes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <PrimeiroComponente/>
   <LifeCycle/>
+  <PessoaComp/>
 </template>
 
 <script>
   import PrimeiroComponente from './components/PrimeiroComponente.vue'
   import LifeCycle from './components/LifeCycle.vue'
+  import PessoaComp from './components/PessoaComp.vue'
 
   export default {
     name: 'AppA',
     components: {
       PrimeiroComponente,
-      LifeCycle
+      LifeCycle,
+      PessoaComp
     }
   }
 
---
+++ src/components/PessoaComp.vue
@@ -0,0 +1,19 @@
+<template>
+    <h2>Esta é a descrição de pessoa: {{nome}}</h2>
+    <InfoUser/>
+</template>
+
+<script>
+import InfoUser from "./InfoUser"
+export default {
+    name: "PessoaComp",
+    components:{
+        InfoUser
+    },
+    data() {
+        return{
+            nome: 'Mateus'
+        }
+    },
+}
+</script>
