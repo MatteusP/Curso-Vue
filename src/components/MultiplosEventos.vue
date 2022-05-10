69caa5dedddc88ecbe9884af32cad03cede16ac9
Mateus Paiva <mattheuspaaiva@gmail.com>
2022-05-10 09:43:26 -0300

    multiplos eventos

---
+++ src/components/MultiplosEventos.vue
@@ -0,0 +1,26 @@
+-+7/*<template>
+    <div>
+        <button @click="primeiro($event), segundo($event)">Ativar Multiplos eventos</button>
+        <p>{{multiplos_eventos }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MultiplosEventos',
+    data() {
+        return{
+            multiplos_eventos: ""
+        }
+    },
+    //executar varios metodos apenas com um click
+    methods:{
+        primeiro(){
+            this.multiplos_eventos = "primeiro evento"
+        },
+        segundo(){
+            setTimeout(()=> this.multiplos_eventos = "segundo evento", 500)
+        }
+    }
+}
+</script>
--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -1,16 +1,21 @@
 <template>
   <h1>Olá,</h1>
   <p>Meu nome é {{nome}} e trabalho como {{profissao}}!</p>
+  <MultiplosEventos/>
 </template>
 
 <script>
+import MultiplosEventos from './MultiplosEventos.vue'
 export default {
-  name: 'PrimeiroComponente',
-  data(){
-    return {
-      nome: 'Mateus',
-      profissao: 'Programador'
+    name: "PrimeiroComponente",
+    data() {
+        return {
+            nome: "Mateus",
+            profissao: "Programador"
+        };
+    },
+    components: { 
+      MultiplosEventos 
     }
-  }
 }
 </script>
